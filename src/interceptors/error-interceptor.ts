b5cfb526ae650b92ad3b641fdea9acf15cfdd11b
Franciole Bezerra <42249453+franciole@users.noreply.github.com>
2020-05-03 19:07:01 -0300

    Revert "Git commit"
    This reverts commit fd784be2b5be7ab06a07a73a85a651c47d25fd5c.

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,17 +2,15 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
-import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService, public alertCtrl: AlertController) { }
+    constructor(public storage: StorageService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
             .catch((error, caught) => {
-
                 let errorObj = error;
                 if (errorObj.error) {
                     errorObj = errorObj.error;
@@ -25,50 +23,16 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch (errorObj.status) {
-                    case 401:
-                        return this.handle401();
-
                     case 403:
                         return this.handle403();
-
-                    default:
-                        return this.handleDefaultError(errorObj);
                 }
 
                 return Observable.throw(errorObj);
             }) as any;
     }
 
-    handle401() {
-        let alert = this.alertCtrl.create({
-            title: 'Erro 401: falha na autenticação',
-            message: 'Email o senha incorretos',
-            enableBackdropDismiss: false,
-            buttons: [
-                {
-                    text: 'OK'
-                }
-            ]
-        });
-        alert.present();
-    }
-
     handle403() {
         this.storage.setLocalUser(null);
-    }
-
-    handleDefaultError(errorObj) {
-        let alert = this.alertCtrl.create({
-            title: 'Erro' + errorObj.status + ": " + errorObj.error,
-            message: errorObj.message,
-            enableBackdropDismiss: false,
-            buttons: [
-                {
-                    text: 'OK'
-                }
-            ]
-        });
-        alert.present();
     }
 }
 
